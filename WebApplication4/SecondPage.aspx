e63c9df5ae6080e3cf8718c399076a5a98af3a71
Corentin Thevenet <corentin.thevenet.efrei@gmail.com>
2014-05-06 23:39:15 +0200

    Add wizard tool

--- WebApplication4/SecondPage.aspx
+++ WebApplication4/SecondPage.aspx
@@ -4,14 +4,30 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+
+    
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
+    
     
         <asp:Button ID="Button1" runat="server" Text="SeondButton" />
-    
+    <div>
+        <asp:Label runat="server">Formulaire d'inscription</asp:Label>
+        <br />
+        <asp:Label runat="server">Prénom : </asp:Label>
+        <br />
+        <asp:Label runat="server">Nom : </asp:Label>
+        <br />
+        <asp:CreateUserWizard ID="CreateUserWizard1" runat="server">
+            <WizardSteps>
+                <asp:CreateUserWizardStep ID="CreateUserWizardStep1" runat="server">
+                </asp:CreateUserWizardStep>
+                <asp:CompleteWizardStep ID="CompleteWizardStep1" runat="server">
+                </asp:CompleteWizardStep>
+            </WizardSteps>
+        </asp:CreateUserWizard>
     </div>
     </form>
 </body>
--- WebApplication4/SecondPage.aspx.designer.cs
+++ WebApplication4/SecondPage.aspx.designer.cs
@@ -29,5 +29,32 @@ namespace WebApplication4 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// CreateUserWizard1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CreateUserWizard CreateUserWizard1;
+        
+        /// <summary>
+        /// CreateUserWizardStep1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CreateUserWizardStep CreateUserWizardStep1;
+        
+        /// <summary>
+        /// CompleteWizardStep1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompleteWizardStep CompleteWizardStep1;
     }
 }
